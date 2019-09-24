f25c2e563c59e08bbdac9c5cd263e0ee219d3e83
William Sedlacek <wsedlacekc@gmail.com>
2019-09-24 18:45:39 -0500

    Remove Debug Logging

--- src/app/state/auth/auth.actions.js
+++ src/app/state/auth/auth.actions.js
@@ -9,7 +9,6 @@ export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS';
 export const SIGNUP_FAILURE = 'SIGNUP_FAILURE';
 
 export const loginAction = credentials => dispatch => {
-  console.log('i was here');
   dispatch({ type: LOGIN_START });
 
   return axios
