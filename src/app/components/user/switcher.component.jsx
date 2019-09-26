dbd59dd2a7c283878ddc3e599854f8e940161418
Connor Holly <connorholly11@gmail.com>
2019-09-25 23:45:12 -0400

    done with switch component design, adding sign out functionality

--- src/app/components/user/switcher.component.jsx
+++ src/app/components/user/switcher.component.jsx
@@ -2,8 +2,30 @@
 import React from 'react';
 import { signout } from '../../state/auth/auth.actions';
 import { useDispatch, useStore } from 'react-redux';
+import { makeStyles } from '@material-ui/core/styles';
+import { NavLink } from 'react-router-dom';
+
+const useStyles = makeStyles({
+  container: {
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+  },
+  // username: {
+  //   display: 'flex',
+  //   justifyContent: 'center',
+  //   width: '25%',
+  // },
+  signout: {
+    textDecoration: 'none',
+    alignItems: 'center',
+    color: 'black',
+  },
+});
 
 const UserSwitcher = () => {
+  const classes = useStyles();
+
   const [user, setUser] = React.useState();
   const dispatch = useDispatch();
   const store = useStore();
@@ -16,9 +38,11 @@ const UserSwitcher = () => {
   console.log('set user', user);
 
   return (
-    <section>
-      <h1> HELLO I'M SWITCHER COMPONENT</h1>
-      {user && user.username}
+    <section className={classes.container}>
+      <h1 className={classes.username}> User: {user && user.username}</h1>
+      <h1>
+        <NavLink className={classes.signout}> Sign Out </NavLink>
+      </h1>
     </section>
   );
 };
